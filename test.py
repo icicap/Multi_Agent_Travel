720d07afde8f86f7968b610d37d19ebe4752b645
icicap <icicap@gmail.com>
2026-07-05 19:29:54 +0530

    app added

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
+
 #res = tavily_search("Best hotels in india")
 #print(res)
 
-res = search_flights("Plan a 7 days Nepal trip from India")
-print(res)
+#res = search_flights("Plan a 7 days Nepal trip from India")
+#print(res)
 
+use_input = input("Enter travel request:")
+response = run_travel_agent(
+    user_input=use_input,
+    thread_id="test_user"
+)
+
+print("\nFinal Response:\n")
+print(response["answer"])
